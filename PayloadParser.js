9d6eb4fbf9b2d909758cb44ffa70ce90dcc2d69f
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-04-16 17:44:43 -0300

    Initial commit
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ PayloadParser.js
@@ -0,0 +1,75 @@
+function parseUplink(device, payload) 
+{
+    var data = milesightDeviceDecode(payload.asBytes(), 85);
+
+
+    var pressure = data.pressure;
+
+   
+    var liters = (75 / 467) * pressure;
+
+  
+    device.endpoints.byAddress("1").updateGenericSensorStatus(pressure);
+
+    device.endpoints.byAddress("2").updateGenericSensorStatus(Number(liters.toFixed(2)));
+
+}
+
+
+function buildDownlink(device, endpoint, command, payload) 
+{ 
+	
+
+}
+
+function milesightDeviceDecode(bytes) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+
+        // BATTERY
+        if (channel_id === 0x01 && channel_type === 0x75) {
+            decoded.battery = bytes[i];
+            i += 1;
+        }
+        // PRESSURE
+        else if (channel_id === 0x03 && channel_type === 0x7b) {
+            decoded.pressure = readInt16LE(bytes.slice(i, i + 2));
+            i += 2;
+        }
+        // HISTROY DATA
+        else if (channel_id === 0x20 && channel_type === 0xce) {
+            var point = {};
+            point.timestamp = readUInt32LE(bytes.slice(i, i + 4));
+            point.pressure = readInt16LE(bytes.slice(i + 4, i + 6));
+
+            decoded.history = decoded.history || [];
+            decoded.history.push(point);
+            i += 6;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
+
+/* ******************************************
+ * bytes to number
+ ********************************************/
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
+function readUInt32LE(bytes) {
+    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+    return value & 0xffffffff;
+}
